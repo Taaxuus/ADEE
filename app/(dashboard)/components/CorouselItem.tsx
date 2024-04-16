1d48913bdacef626b6454b7aba74ed6212f75139
Cezary Draus <108001877+Taaxuus@users.noreply.github.com>
2024-04-16 13:14:42 +0200

    work

---
+++ app/(dashboard)/components/CorouselItem.tsx
@@ -0,0 +1,49 @@
+"use client";
+import React from "react";
+import { FaInfoCircle } from "react-icons/fa";
+import { useState } from "react";
+
+interface Props {
+	project: {
+		name: string;
+		bg: string; // This should be a URL path if it's used for backgrounds
+		description: string;
+	};
+}
+
+export default function CarouselItem({ project }: Props) {
+	const [infoShow, setInfoShow] = useState(false);
+
+	return (
+		<div className="w-full h-screen flex relative justify-center items-center">
+			<div
+				style={{
+					backgroundImage: `url(${project.bg})`,
+					backgroundSize: "cover",
+					backgroundPosition: "center",
+				}}
+				className="w-full h-full flex justify-center items-center">
+				<div className="text-white text-2xl font-bold p-10">{project.name}</div>
+				<div
+					onMouseEnter={() => setInfoShow(true)}
+					onMouseLeave={() => setInfoShow(false)}
+					className="absolute bottom-2 right-0 mr-5 z-50">
+					<p className="text-2xl hover:scale-110 transition-transform duration-300 cursor-help">
+						<FaInfoCircle />
+					</p>
+				</div>
+
+				{infoShow && (
+					<div
+						className={`absolute h-full w-full left-0 top-0 ${
+							infoShow ? "bg-black bg-opacity-80" : ""
+						}  transition-opacity duration-1000 ease-in-out`}>
+						<div className="text-white text-xl p-10 transition-opacity duration-1000 ease-in-out">
+							{project.description}
+						</div>
+					</div>
+				)}
+			</div>
+		</div>
+	);
+}
--- app/(dashboard)/components/Projects.tsx
+++ app/(dashboard)/components/Projects.tsx
@@ -1,20 +1,76 @@
 "use client";
-import React from "react"; // Ensure React is imported
-import { Carousel } from "nuka-carousel";
+
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import { projects } from "../../utilities/projectsObject";
-import CarouselItem from "./CorouselItem2";
+import CarouselItem from "./CorouselItem";
 
 export default function Projects() {
+	const settings = {
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		cssEase: "linear",
+		responsive: [
+			{
+				breakpoint: 1024, // at max-width 1024px
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+					infinite: true,
+					dots: true,
+				},
+			},
+			{
+				breakpoint: 768, // at max-width 600px
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1,
+					initialSlide: 1,
+				},
+			},
+			{
+				breakpoint: 600, // at max-width 600px
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					initialSlide: 1,
+				},
+			},
+			{
+				breakpoint: 600, // at max-width 600px
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					initialSlide: 1,
+				},
+			},
+			{
+				breakpoint: 480, // at max-width 480px
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				},
+			},
+		],
+	};
+
 	return (
 		<section
 			id="projects"
 			className="w-full flex flex-col h-screen p-0 overflow-hidden">
-			<Carousel className="w-full" autoplay={true} autoplayInterval={3000}>
+			<Slider {...settings}>
 				{projects.map((project) => (
 					<CarouselItem key={project.name} project={project} />
 				))}
-			</Carousel>
+			</Slider>
 		</section>
 	);
 }
